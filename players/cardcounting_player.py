87e246fc08e489ae99d594b41a6fbc190250b944
Adrian Schneider <adrian.schneider.pr@gmail.com>
2025-09-17 15:26:12 +0200

    memorize cards

--- players/cardcounting_player.py
+++ players/cardcounting_player.py
@@ -7,18 +7,22 @@ class CardCountingPlayer(Player):
 
     def __init__(self):
         super().__init__()
+        self.played_cards = {}
 
-    def call_select_card(self, cards: List[Tuple[int, int]], board: List[List[Tuple[int, int]]], all_penalty_cards: List[List[Tuple[int, int]]]) -> int:
+    def call_select_card(self, own_cards: List[Tuple[int, int]], board: List[List[Tuple[int, int]]], all_penalty_cards: List[List[Tuple[int, int]]]) -> int:
         # this player memorizes all played cards and computes an accurate probability
         # in order to not hit the sixth card
 
-
+        self._memorize_played_cards(own_cards, board, all_penalty_cards)
 
         # todo: implement
         return 0
 
-    def _memorize_played_cards(self, board: List[List[Tuple[int, int]]]) -> None:
-        pass
+    def _memorize_played_cards(self, own_cards: List[Tuple[int, int]], board: List[List[Tuple[int, int]]], all_penalty_cards: List[List[Tuple[int, int]]]) -> None:
+        for ll in [[own_cards], board, all_penalty_cards]:
+            for l in ll:
+                for n, _ in l:
+                    self.played_cards[n] = True
 
     def _was_card_played(self, card_num: int) -> bool:
-        pass
+        return card_num in self.played_cards
--- test/test_players.py
+++ test/test_players.py
@@ -21,8 +21,28 @@ class TestPlayers(unittest.TestCase):
         m_player = CardCountingPlayer()
         cards = [(10, 1), (20, 1), (30, 1)]
         boards = [[(15, 1), (16, 1)], [(19, 1)], [(50, 1), (51, 1)], [(60, 1), (61, 1)]]
-        all_penalty_cards = [[(90, 1)], [], [], [(95, 1), (98, 1)]]
-        pass
+        all_penalty_cards = [[(90, 1)], [], [], [(95, 1), (98, 1)]] # they were seen when played
+
+        # before play, no cards were seen
+        for i in range(1, 105):
+            self.assertFalse(m_player._was_card_played(i))
+
+        _ = m_player.call_select_card(cards, boards, all_penalty_cards)
+
+        # check if all cards were memorized which are visible to the player
+        played_cards = [(10, 1), (20, 1), (30, 1), (15, 1), (16, 1), (19, 1), (50, 1), (51, 1), (60, 1), (61, 1), (90, 1), (95, 1), (98, 1)]
+
+        for n, p in played_cards:
+            self.assertTrue(m_player._was_card_played(n))
+
+        # add a new card and check again
+        boards2 = [[(13, 1), (16, 1)], [(19, 1)], [(50, 1), (51, 1)], [(60, 1), (61, 1)]]
+        _ = m_player.call_select_card(cards, boards2, all_penalty_cards)
+        played_cards2 = [(10, 1), (20, 1), (30, 1), (15, 1), (16, 1), (19, 1), (50, 1), (51, 1), (60, 1), (61, 1),
+                        (90, 1), (95, 1), (98, 1), (13, 1)]
+
+        for n, p in played_cards2:
+            self.assertTrue(m_player._was_card_played(n))
 
 
 
